906af03cac2481ec62dedaf76b2caefc8882cd28
Ali Khaledi Nasab <ali.khaledi1989@gmail.com>
2017-11-14 17:11:12 -0500

    Add files via upload

--- choose_random_tree.f90
+++ choose_random_tree.f90
@@ -1,30 +1,43 @@
 
 program chooose_random_tree
 
-    character(len=15) :: c3
-    integer :: i
+    character(len=15) :: tree_file
+    integer :: i,i2,i5,node,nh,i1
     real*8:: a2
-
+    integer,allocatable, dimension(:):: nlink,hemind
+    integer,allocatable, dimension(:,:)::link,adj
+    
      call random_seed()
      call random_number(a2)
-     i= int(a2*10+1)  ! i is the number of file 
-     call rand_tree_name(i,c3)  ! this subroutine create the name 'i.tree'
+     node=0
+     nh=0
+     
+     i1= int(a2*5+1)  ! i is the number of file 
+     call rand_tree_name(i1,tree_file)  ! this subroutine create the name 'i.tree'
 
-        open(unit=11,file=c3)  ! open the flie and read it 
-        read(11,*) i2
-        read(11,*)i5
-        print*,i2,i5
- !   enddo
-    
-
+     open(unit=11,file=tree_file)  ! open the flie and read it 
+     read(11,*) node
+     read(11,*)nh
+     allocate(nlink(node),link(node,node),hemind(nh),adj(node,node))
+     
+     do i=1,node
+        do j=1, node
+           read(11,*) adj(j,i)
+        end do 
+     end do   
+     print*,i1,node,nh
+     
+     call adj_locations(nh,node,adj,link,nlink,hemind)
+     deallocate(nlink,link,hemind,adj)
+     
 end program
 
 
-subroutine rand_tree_name(i,c3)
-
-integer,intent(in):: i 
-character(len=10) :: fmt
-character(len=15) :: filename,c3
+subroutine rand_tree_name(i,tree_file)
+ implicit none
+ integer,intent(in):: i 
+ character(len=10) :: fmt
+ character(len=15) :: filename,tree_file
 
     if (i .lt. 10) then   
         fmt = "(I1,A5)"  
@@ -42,8 +55,44 @@ character(len=15) :: filename,c3
          fmt = "(I5,A5)" 
       endif
       
-       write (filename,fmt) i,".tree"
-        c3=trim(filename)
+      write (filename,fmt) i,".tree"
+      tree_file=trim(filename)
  
 return 
 end 
+
+ subroutine adj_locations(nh,node,adj,link,nlink,hemind)
+    implicit none 
+    integer,intent(in):: nh,node
+    integer,intent(in):: adj(node,node)
+    integer,intent(out):: nlink(node),link(node,node),hemind(nh)
+    integer:: f,i,j,k,k3
+    nlink=0
+    link=0
+    hemind=0
+    
+     k3=0
+     do i=node,2,-1 ! look for the location of the peripherals start from the last node and go backward until the 2, 
+                                                      !to aviod confusing the central one as heminode in case it has the branching=1.  
+         if(sum(adj(i,1:node))==1) then ! the sum of adj is equl to 1 then we have a heminode
+             k3=k3+1
+             hemind(k3)=node-k3+1  !number the heminodes
+         end if  
+     enddo
+
+       link=0
+       do i=1,node
+          k=0
+          do j=1,node
+             if(adj(i,j).eq.1) then
+                k=k+1
+                link(i,k)=j
+             endif
+          enddo
+          nlink(i)=k
+       enddo
+    return 
+    end 
+
+
+ 
